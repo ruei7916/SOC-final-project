2ca7e2afa6510e659449e4555f7e76f8f3631b0b
Ricky Li <ruei7916@gmail.com>
2024-01-15 23:54:44 +0800

    fix coding style

--- rtl/user/arbiter.v
+++ rtl/user/arbiter.v
@@ -1,94 +1,101 @@
 module arbiter(
-    input wb_clk_i,
-    input wb_rst_i,
-    input wb_ack_o,
+    input clk,
+    input rst,
+	// to cpu
     input cpu_stb_i,
     input cpu_cyc_i,
     input cpu_we_i,
     input [3:0] cpu_sel_i,
     input [31:0] cpu_dat_i,
     input [31:0] cpu_adr_i,
+    output reg cpu_ack_o,
+	// to dma
     input dma_stb_i,
     input dma_cyc_i,
     input dma_we_i,
     input [3:0] dma_sel_i,
     input [31:0] dma_dat_i,
     input [31:0] dma_adr_i,
-    output reg wb_stb_i,
-    output reg wb_cyc_i,
-    output reg wb_we_i,
-    output reg [3:0] wb_sel_i,
-    output reg [31:0] wb_dat_i,
-    output reg [31:0] wb_adr_i,
-    output reg cpu_ack_o,
     output reg dma_ack_o,
+	// to sdram
+    input sdram_ack_o,
+    output reg sdram_stb_i,
+    output reg sdram_cyc_i,
+    output reg sdram_we_i,
+    output reg [3:0] sdram_sel_i,
+    output reg [31:0] sdram_dat_i,
+    output reg [31:0] sdram_adr_i,
+
     output [31:0] wbs_dat_o
 );
-reg busy_d,busy_q;
-	always@(*)begin
-		if(dma_stb_i & dma_cyc_i & ~busy_q)begin
-			busy_d = 1;
-			wb_stb_i = dma_stb_i;
-			wb_cyc_i = dma_cyc_i;
-			wb_we_i = dma_we_i;
-			wb_sel_i = dma_sel_i;
-    			wb_dat_i = dma_dat_i;
-    			wb_adr_i = dma_adr_i;
-    			dma_ack_o = wb_ack_o;
-    			cpu_ack_o = 0;	
-    		end
-    		else if(dma_stb_i & dma_cyc_i & busy_q & wb_ack_o)begin
-			busy_d = 0;
-			wb_stb_i = dma_stb_i;
-			wb_cyc_i = dma_cyc_i;
-			wb_we_i = dma_we_i;
-			wb_sel_i = dma_sel_i;
-    			wb_dat_i = dma_dat_i;
-    			wb_adr_i = dma_adr_i;
-    			dma_ack_o = wb_ack_o;
-    			cpu_ack_o = 0;
-    		end
-    		else if(cpu_stb_i & cpu_cyc_i & busy_q & wb_ack_o)begin
-			busy_d = 0;
-			wb_stb_i = cpu_stb_i;
-			wb_cyc_i = cpu_cyc_i;
-			wb_we_i = cpu_we_i;
-			wb_sel_i = cpu_sel_i;
-    			wb_dat_i = cpu_dat_i;
-    			wb_adr_i = cpu_adr_i;
-    			cpu_ack_o = wb_ack_o;
-    			dma_ack_o = 0;
-    		end
-    		else if(cpu_stb_i & cpu_cyc_i & ~busy_q)begin
-			busy_d = 1;
-			wb_stb_i = cpu_stb_i;
-			wb_cyc_i = cpu_cyc_i;
-			wb_we_i = cpu_we_i;
-			wb_sel_i = cpu_sel_i;
-    			wb_dat_i = cpu_dat_i;
-    			wb_adr_i = cpu_adr_i;
-    			cpu_ack_o = wb_ack_o;
-    			dma_ack_o = 0;
-    		end
-    		else begin
-    			busy_d = busy_q;
-			wb_stb_i = cpu_stb_i;
-			wb_cyc_i = cpu_cyc_i;
-			wb_we_i = cpu_we_i;
-			wb_sel_i = cpu_sel_i;
-    			wb_dat_i = cpu_dat_i;
-    			wb_adr_i = cpu_adr_i;
-    			cpu_ack_o = wb_ack_o;
-    			dma_ack_o = 0;
-    		end		
+
+reg busy_d, busy_q;
+
+always@(*)begin
+	if(dma_stb_i & dma_cyc_i & ~busy_q)begin
+		busy_d = 1;
+		sdram_stb_i = dma_stb_i;
+		sdram_cyc_i = dma_cyc_i;
+		sdram_we_i = dma_we_i;
+		sdram_sel_i = dma_sel_i;
+		sdram_dat_i = dma_dat_i;
+		sdram_adr_i = dma_adr_i;
+        dma_ack_o = sdram_ack_o;
+        cpu_ack_o = 0;	
+    end
+    else if(dma_stb_i & dma_cyc_i & busy_q & sdram_ack_o)begin
+		busy_d = 0;
+		sdram_stb_i = dma_stb_i;
+		sdram_cyc_i = dma_cyc_i;
+		sdram_we_i = dma_we_i;
+		sdram_sel_i = dma_sel_i;
+        sdram_dat_i = dma_dat_i;
+        sdram_adr_i = dma_adr_i;
+        dma_ack_o = sdram_ack_o;
+        cpu_ack_o = 0;
+    end
+    else if(cpu_stb_i & cpu_cyc_i & busy_q & sdram_ack_o)begin
+        busy_d = 0;
+        sdram_stb_i = cpu_stb_i;
+		sdram_cyc_i = cpu_cyc_i;
+		sdram_we_i = cpu_we_i;
+		sdram_sel_i = cpu_sel_i;
+        sdram_dat_i = cpu_dat_i;
+        sdram_adr_i = cpu_adr_i;
+        cpu_ack_o = sdram_ack_o;
+        dma_ack_o = 0;
+    end
+    else if(cpu_stb_i & cpu_cyc_i & ~busy_q)begin
+        busy_d = 1;
+        sdram_stb_i = cpu_stb_i;
+		sdram_cyc_i = cpu_cyc_i;
+		sdram_we_i = cpu_we_i;
+		sdram_sel_i = cpu_sel_i;
+        sdram_dat_i = cpu_dat_i;
+        sdram_adr_i = cpu_adr_i;
+        cpu_ack_o = sdram_ack_o;
+        dma_ack_o = 0;
+    end
+    else begin
+        busy_d = busy_q;
+		sdram_stb_i = cpu_stb_i;
+		sdram_cyc_i = cpu_cyc_i;
+		sdram_we_i = cpu_we_i;
+		sdram_sel_i = cpu_sel_i;
+        sdram_dat_i = cpu_dat_i;
+        sdram_adr_i = cpu_adr_i;
+        cpu_ack_o = sdram_ack_o;
+        dma_ack_o = 0;
+    end
+end
+
+always@(posedge clk or posedge rst)begin
+    if(rst)begin
+        busy_q <= 0;
 	end
-    	always@(posedge wb_clk_i or posedge wb_rst_i)begin
-    		if(wb_rst_i)begin
-    			busy_q <= 0;
-		end
-		else begin
-			busy_q <= busy_d;
-		end
-		
+	else begin
+		busy_q <= busy_d;
 	end
+end
+
 endmodule
--- rtl/user/user_proj_wrapper_all.v
+++ rtl/user/user_proj_wrapper_all.v
@@ -113,7 +113,7 @@ wire dma_we;
 wire dma_ack_o;
 wire [3:0]dma_sel;
 wire [31:0]dma_dat_i;
-
+//
 wire ack_o;
 wire stb_i;
 wire cyc_i;
@@ -135,21 +135,21 @@ assign ctrl_addr = adr_i[22:0];
 // uart
 assign decoded_uart = (wbs_adr_i[31:20] == 12'h300) ? 1'b1 : 1'b0;
 assign rst_n = ~rst;
-    always @(posedge clk) begin
-        if (rst) begin
-            ctrl_in_valid_q <= 1'b0;
-        end
-        else begin
-            if (~wbs_we_i && valid && ~ctrl_busy && ctrl_in_valid_q == 1'b0)
-                ctrl_in_valid_q <= 1'b1;  
-            else if (ctrl_out_valid)
-                ctrl_in_valid_q <= 1'b0;
-        end
+always @(posedge clk) begin
+    if (rst) begin
+        ctrl_in_valid_q <= 1'b0;
     end
-    arbiter u_arbiter(
-    .wb_clk_i(clk),
-    .wb_rst_i(rst),
-    .wb_ack_o(ack_o),
+    else begin
+        if (~wbs_we_i && valid && ~ctrl_busy && ctrl_in_valid_q == 1'b0)
+            ctrl_in_valid_q <= 1'b1;  
+        else if (ctrl_out_valid)
+            ctrl_in_valid_q <= 1'b0;
+    end
+end
+arbiter u_arbiter(
+    .clk(clk),
+    .rst(rst),
+    .sdram_ack_o(ack_o),
     .cpu_stb_i(wbs_stb_i),
     .cpu_cyc_i(wbs_cyc_i),
     .cpu_we_i(wbs_we_i),
@@ -162,77 +162,51 @@ assign rst_n = ~rst;
     .dma_sel_i(dma_sel),
     .dma_dat_i(32'd1),
     .dma_adr_i(dma_addr),
-    .wb_stb_i(stb_i),
-    .wb_cyc_i(cyc_i),
-    .wb_we_i(we_i),
-    .wb_sel_i(sel_i),
-    .wb_dat_i(dat_i),
-    .wb_adr_i(adr_i),
+    .sdram_stb_i(stb_i),
+    .sdram_cyc_i(cyc_i),
+    .sdram_we_i(we_i),
+    .sdram_sel_i(sel_i),
+    .sdram_dat_i(dat_i),
+    .sdram_adr_i(adr_i),
     .cpu_ack_o(wbs_ack_o),
     .dma_ack_o(dma_ack_o),
     .wbs_dat_o(wbs_dat_o)
-    );
-    dma DMA(
-    .wb_clk_i(clk),
-    .wb_rst_i(rst),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .read_dat_i(wbs_dat_o),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_ack(ack_o),
-    .dma_ack(dma_ack_o),
-    .ss_tdata(ss_tdata),
-    .wbs_adr_o(dma_addr),
-    .wbs_stb_o(dma_stb),
-    .wbs_cyc_o(dma_cyc),
-    .wbs_we_o(dma_we),
-    .wbs_sel_o(dma_sel),
-    .ss_tvalid,
-    .ss_tready,
-    .sm_tvalid,
-    .sm_tready,
-    .sm_tdata
-    );
-    sdram_controller user_sdram_controller (
-        .clk(clk),
-        .rst(rst),
-        
-        .sdram_cle(sdram_cle),
-        .sdram_cs(sdram_cs),
-        .sdram_cas(sdram_cas),
-        .sdram_ras(sdram_ras),
-        .sdram_we(sdram_we),
-        .sdram_dqm(sdram_dqm),
-        .sdram_ba(sdram_ba),
-        .sdram_a(sdram_a),
-        .sdram_dqi(d2c_data),
-        .sdram_dqo(c2d_data),
-
-        .user_addr(ctrl_addr),
-        .rw(wbs_we_i),
-        .data_in(wbs_dat_i),
-        .data_out(wbs_dat_o),
-        .busy(ctrl_busy),
-        .in_valid(ctrl_in_valid),
-        .out_valid(ctrl_out_valid)
-    );
-
-    sdr user_bram (
-        .Rst_n(rst_n),
-        .Clk(clk),
-        .Cke(sdram_cle),
-        .Cs_n(sdram_cs),
-        .Ras_n(sdram_ras),
-        .Cas_n(sdram_cas),
-        .We_n(sdram_we),
-        .Addr(sdram_a),
-        .Ba(sdram_ba),
-        .Dqm(bram_mask),
-        .Dqi(c2d_data),
-        .Dqo(d2c_data)
-    );
+);
+sdram_controller user_sdram_controller (
+    .clk(clk),
+    .rst(rst),
+    .sdram_cle(sdram_cle),
+    .sdram_cs(sdram_cs),
+    .sdram_cas(sdram_cas),
+    .sdram_ras(sdram_ras),
+    .sdram_we(sdram_we),
+    .sdram_dqm(sdram_dqm),
+    .sdram_ba(sdram_ba),
+    .sdram_a(sdram_a),
+    .sdram_dqi(d2c_data),
+    .sdram_dqo(c2d_data),
+    .user_addr(ctrl_addr),
+    .rw(wbs_we_i),
+    .data_in(wbs_dat_i),
+    .data_out(wbs_dat_o),
+    .busy(ctrl_busy),
+    .in_valid(ctrl_in_valid),
+    .out_valid(ctrl_out_valid)
+);
+sdr user_bram (
+    .Rst_n(rst_n),
+    .Clk(clk),
+    .Cke(sdram_cle),
+    .Cs_n(sdram_cs),
+    .Ras_n(sdram_ras),
+    .Cas_n(sdram_cas),
+    .We_n(sdram_we),
+    .Addr(sdram_a),
+    .Ba(sdram_ba),
+    .Dqm(bram_mask),
+    .Dqi(c2d_data),
+    .Dqo(d2c_data)
+);
 
 uart uart (
 `ifdef USE_POWER_PINS
